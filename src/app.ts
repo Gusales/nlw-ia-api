085fb7538b86eab9a2e256aa5736ad8983bda080
= <=>
2023-09-13 10:25:31 -0300

    Feat: Finish aplication structure
    Create all routes, integration with openai AI completed and configure fastify cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
 import fastify from "fastify";
-import { appRoutes } from "./routes";
+import { fastifyCors } from '@fastify/cors'
 
+import { getAllPrompts } from './routes/get-all-prompts'
+import { uploadVideosRoutes } from "./routes/upload-videos";
+import { createTranscriptionRoutes } from "./routes/create-transcription";
+import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
+import { env } from "./env";
+ 
 export const app = fastify()
 
-app.register(appRoutes)
+app.register(getAllPrompts)
+app.register(uploadVideosRoutes)
+app.register(createTranscriptionRoutes)
+app.register(generateAiCompletionRoute)
+
+app.register(fastifyCors, {
+  origin: [
+    env.NODE_ENV === "dev" ? 'http://localhost:5173' : '', // PRODUCTION URL HERE
+  ],
+})
